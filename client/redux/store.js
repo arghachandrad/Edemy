a0206029fd7fc6e27589e1ec8219744c7829e76b
arghachdas <arghachandra@technoexponent.com>
2022-02-09 23:45:27 +0530

    root reducer added

--- client/redux/store.js
+++ client/redux/store.js
@@ -1,11 +1,7 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit"
-import authReducer from "./auth/reducer"
+import { rootReducer } from "./rootReducer"
 
 export const store = configureStore({
-  reducer: {
-    // This is where we add reducers.
-    // Since we don't have any yet, leave this empty
-    auth: authReducer,
-  },
+  reducer: rootReducer,
   devTools: process.env.NODE_ENV !== "production",
 })
