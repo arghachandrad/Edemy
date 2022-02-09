c0e154994a59adf8c2512a59c5a505b1312d4fd4
arghachdas <arghachandra@technoexponent.com>
2022-02-10 01:30:18 +0530

    redux persist added for persisting state data on loading

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -7,9 +7,10 @@ import { Provider } from "react-redux"
 import "/styles/globals.css"
 import theme from "../styles/theme"
 import TopNav from "../components/TopNav"
-import { store } from "../redux/store"
+import { persistor, store } from "../redux/store"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
+import { PersistGate } from "redux-persist/integration/react"
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
@@ -24,20 +25,22 @@ export default function MyApp(props) {
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <TopNav />
-          <ToastContainer
-            position="top-center"
-            autoClose={3000}
-            hideProgressBar={true}
-            newestOnTop={false}
-            closeOnClick
-            theme="colored"
-          />
-          <Component {...pageProps} />
-        </ThemeProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <TopNav />
+            <ToastContainer
+              position="top-center"
+              autoClose={3000}
+              hideProgressBar={true}
+              newestOnTop={false}
+              closeOnClick
+              theme="colored"
+            />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </PersistGate>
       </Provider>
     </CacheProvider>
   )
